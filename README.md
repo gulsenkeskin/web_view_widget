3e05640693328c4127adf139bdd035dd19899468
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-03-02 20:55:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# web_view_widget
+ Flutter WebView widget
---
+++ android/app/src/main/kotlin/com/example/web_view_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.web_view_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

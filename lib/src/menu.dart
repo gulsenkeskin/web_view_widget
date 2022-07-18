9922ea8f8c584ba87b0144a7c0da2c9d4b428319
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-07-18 23:46:23 +0300

    Cookie yönetimi
    Cookie listesi gösterme, silme ve yeni cookie ler oluşturma

--- lib/src/menu.dart
+++ lib/src/menu.dart
@@ -3,7 +3,16 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-enum _MenuOptions { navigationDelegate, userAgent, javascriptChannel }
+enum _MenuOptions {
+  navigationDelegate,
+  userAgent,
+  javascriptChannel,
+  listCookies,
+  clearCookies,
+  addCookie,
+  setCookie,
+  removeCookie,
+}
 
 class Menu extends StatefulWidget {
   const Menu({required this.controller, Key? key}) : super(key: key);
@@ -14,6 +23,39 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
+  final CookieManager cookieManager=CookieManager();
+  
+  //tüm cookie'lerin listesini alma
+  Future<void> _onListCookies(WebViewController controller) async{
+    final String cookies= await controller.runJavascriptReturningResult('document.cookie');
+    if(!mounted) return;
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(cookies.isNotEmpty ? cookies : "Mevcut cookie yok")));
+  }
+
+  //tüm cookie'leri temizleme
+ /* WebView'daki tüm tanımlama bilgilerini temizlemek için CookieManager sınıfının clearCookies yöntemini kullanın. Bu method CookieManager tanımlama bilgilerini temizlediyse true, temizlenecek tanımlama bilgisi yoksa false döndürür (future<bool>)*/
+
+Future<void> _onClearCookies()async{
+  final hadCookies=await cookieManager.clearCookies(); //cookie varsa ve temizlendiyse geriye true yoksa false dönen method
+  String message="Cookie'ler temizlendi";
+  if(!hadCookies){
+    message="Temizlenecek Cookie yok";
+  }
+  if(!mounted) return;
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+}
+
+//cookie ekleme //javascript ile yapılır
+  Future<void> _onAddCookie(WebViewController controller) async{
+  await controller.runJavascript('''var date= new Date();
+ date.setTime(date.getTime()+(30*24*60*60*1000));
+  document.cookie = "FirstName=Gülseeeen; expires=" + date.toGMTString();''');
+  if(!mounted) return;
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("cookie eklendi")));
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<WebViewController>(
